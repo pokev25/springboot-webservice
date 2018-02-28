ab41961e4618155fc9f58f8c922d89642b08fdff
pokev25 <pokev25@gmail.com>
2018-02-28 19:40:57 +0900

    jpa로 간단 api 만들기 - java8의 LocalDate를 사용할 때 Hibernate core 버그 해결된 버전을 사용해야함

---
+++ src/main/java/com/ssc/webservice/dto/posts/PostsSaveRequestDto.java
@@ -0,0 +1,23 @@
+package com.ssc.webservice.dto.posts;
+
+import com.ssc.webservice.domain.Posts;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+    private String title;
+    private String content;
+    private String author;
+
+    public Posts toEntity(){
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
--- src/test/java/com/ssc/webservice/ApplicationTests.java
+++ src/test/java/com/ssc/webservice/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ssc.springwebservice;
+package com.ssc.webservice;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
