c5f986b77618e7020e5b74a9357bbd1221e3dfb5
pokev25 <pokev25@gmail.com>
2018-02-28 21:02:41 +0900

    Handlebars로 화면 만들기

---
+++ src/main/java/com/ssc/webservice/service/PostsService.java
@@ -0,0 +1,31 @@
+package com.ssc.webservice.service;
+
+import com.ssc.webservice.domain.PostsRepository;
+import com.ssc.webservice.dto.posts.PostsMainResponseDto;
+import com.ssc.webservice.dto.posts.PostsSaveRequestDto;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@AllArgsConstructor
+@Service
+public class PostsService {
+    private PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto dto){
+        return postsRepository.save(dto.toEntity()).getId();
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsMainResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc()
+                .map(PostsMainResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+
+}
